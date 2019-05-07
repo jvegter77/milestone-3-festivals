7eb8c714430a412171618c3a9dc7245348e7d775
Jeroen Vegter <jeroenvegter@hotmail.com>
2019-05-07 10:57:20 +0000

    connected application to mongodb

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import os
 from flask import Flask, flash, render_template, redirect, request, url_for, session, abort, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'milestone-3-festivals'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-gdxlz.mongodb.net/milestone-3-festivals?retryWrites=true'
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello():

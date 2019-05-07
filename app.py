211405e63ddaa12e09754627cf76d5baa2e3ff50
Jeroen Vegter <jeroenvegter@hotmail.com>
2019-05-07 10:43:58 +0000

    added procfile

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 import os
 from flask import Flask, flash, render_template, redirect, request, url_for, session, abort, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello World...again'
+    
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+    port=int(os.environ.get('PORT')),
+    debug=True)
